f19f3fbba0c908cac16e2d02995f9a45e884e82f
Kripth <kripth04@gmail.com>
2018-11-09 22:02:52 +0100

    Send whether the species is extinct in response to getallsnails/genus

--- src/main/java/com/kasokuz/snaildb/controller/get/response/GetGenusResponse.java
+++ src/main/java/com/kasokuz/snaildb/controller/get/response/GetGenusResponse.java
@@ -3,23 +3,36 @@ package com.kasokuz.snaildb.controller.get.response;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.kasokuz.snaildb.domain.Species;
+
 public class GetGenusResponse extends CommonResponse {
 	
 	public final Integer familyId;
 	
-	public final List<Child> species = new ArrayList<>();
+	public final List<SpeciesChild> species = new ArrayList<>();
 	
 	public GetGenusResponse(com.kasokuz.snaildb.domain.Genus genus) {
 		super(genus.getGenusId(), genus.getName(), genus.getTaxonomers(), genus.getTaxonomyYear());
 		this.familyId = genus.getFamily().getFamilyId();
 		for(com.kasokuz.snaildb.domain.Species species : genus.getSpecies()) {
-			this.species.add(new Child(species.getSpeciesId(), species.getName(), species.getTaxonomers(), species.getTaxonomyYear()));
+			this.species.add(new SpeciesChild(species));
 		}
 	}
 	
 	public static GetGenusResponse from(com.kasokuz.snaildb.domain.Genus genus) {
 		if(genus == null) return null;
 		else return new GetGenusResponse(genus);
+	}
+	
+	public static class SpeciesChild extends Child {
+		
+		public final Boolean extinct;
+		
+		public SpeciesChild(Species species) {
+			super(species.getSpeciesId(), species.getName(), species.getTaxonomers(), species.getTaxonomyYear());
+			this.extinct = species.getExtinct();
+		}
+		
 	}
 
 }
